9f9e4e7b95577f47d431b2d4664b4c5703f8f1ba
Ecnounter0929 <72746075+Ecnounter0929@users.noreply.github.com>
2020-11-16 20:26:27 +0800

    Add files via upload

---
+++ xuexijieduandedaima/pages/demo2/demo2.js
@@ -0,0 +1,75 @@
+// pages/demo2/demo2.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    title:"2020微信小程序开发",
+    content:"欢迎来到德莱联盟",
+    listArr:["平面设计","UI设计","前端开发"],
+    obj:{name:"王进",age:18,sex:"男"},
+    day:8
+  },
+
+  navBack:function(){
+    wx.navigateBack()
+  },
+
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
