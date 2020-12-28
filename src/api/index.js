02bc234cb7df615017fe2e5ef31c134936f7bd38
qiaoke <2627296432@qq.com>
2020-12-28 21:47:54 +0800

    vue

---
+++ src/api/index.js
@@ -0,0 +1,10 @@
+import request from '../utils/request';
+
+export const fetchData = query => {
+    return request({
+        url: './table.json',
+        method: 'get',
+        params: query
+    });
+};
+
